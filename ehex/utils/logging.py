1f63f0416b17966912f6af963178c2d7d4772117
Tonico Strasser <tonico.strasser@gmail.com>
2020-09-11 23:16:06 +0200

    refactor: Rename logger adapter

--- ehex/utils/logging.py
+++ ehex/utils/logging.py
@@ -10,7 +10,7 @@ class Message:
         return str(self.msg).format(*self.args)
 
 
-class StyleAdapter(logging.LoggerAdapter):
+class EhexLogger(logging.LoggerAdapter):
     def log(self, level, msg, /, *args, **kws):
         if self.isEnabledFor(level):
             msg, kws = self.process(msg, kws)
@@ -18,4 +18,4 @@ class StyleAdapter(logging.LoggerAdapter):
 
 
 def get_logger(name="ehex"):
-    return StyleAdapter(logging.getLogger(name), extra=None)
+    return EhexLogger(logging.getLogger(name), extra=None)
